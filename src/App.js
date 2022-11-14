51c6ca1faad4785e1272ee0956eaf54e78651714
audreythando <audreythando@gmail.com>
2022-11-14 20:51:12 +0200

    refactor: restored redux files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-   
+   <h1>Hey Sis</h1>
     </div>
   );
 }
