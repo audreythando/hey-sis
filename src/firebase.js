6846bc4a68b430d24a693285ab1d9a669d54fb78
audreythando <audreythando@gmail.com>
2022-11-14 20:30:48 +0200

    refactor:removed unwanted files and code

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXAXPxt3sAhV0x4vkDupMXTfCnP22tRdg",
+    authDomain: "hey-sis-2d8f6.firebaseapp.com",
+    projectId: "hey-sis-2d8f6",
+    storageBucket: "hey-sis-2d8f6.appspot.com",
+    messagingSenderId: "834769161837",
+    appId: "1:834769161837:web:225b353b92057f77f17964"
+  };
